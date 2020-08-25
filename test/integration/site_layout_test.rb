3ab6d88ee71318c559d33c67e553ad688c9007cb
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-08-24 20:04:40 -0500

    adds assert for the title

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,5 +8,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', help_path
     assert_select 'a[href=?]', about_path
     assert_select 'a[href=?]', contact_path
+    assert_select 'title', full_title
   end
 end
