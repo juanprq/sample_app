871f9b5fef769a08ac007880f9704797ea7182da
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-08-26 20:36:09 -0500

    adds simple user show template

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,4 +5,9 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_response :success
   end
+
+  test "should get show" do
+    get user_path 1
+    assert_response :succes
+  end
 end
