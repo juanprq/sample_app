1f6e83a999aa6fcbdfc4e1f0f71757f0f4151342
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-08-28 21:28:25 -0500

    adds improvements over the signup integration test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -8,5 +8,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       post users_path, params: { user: { name: '', email: 'foo@bar.com', password: 'foo', password_confirmation: 'bar' } }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation', count: 1
+    assert_select 'div.alert.alert-danger', count: 1, value: 'The form contains 6 errors'
   end
 end
