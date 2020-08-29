caa146c35f67c57718e1c12aa9de8d2ed4d62a35
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-08-29 17:01:57 -0500

    adds logic to handle to login action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      flash[:success] = 'Welcome to sample app'
+      redirect_to users_show_path(user)
+    else
+      flash[:danger] = 'Invalid credentials, please try again!'
+      render 'new'
+    end
+  end
 end
