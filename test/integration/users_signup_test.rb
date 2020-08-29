9a6e16ebf1b02e8501c457745452802409893e1f
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-08-28 21:19:40 -0500

    adds users signup integration test

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test 'something' do
+    get signup_path
+    assert_select 'input[id=?]', 'user_name', count: 1
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: '', email: 'foo@bar.com', password: 'foo', password_confirmation: 'bar' } }
+    end
+    assert_template 'users/new'
+  end
+end
