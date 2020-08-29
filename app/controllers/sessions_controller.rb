940f7ef1a42d64ff7af9148fb920c78e73bc93af
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-08-29 18:06:19 -0500

    adds helper methods for logged in users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      flash[:success] = 'Welcome to sample app'
-      redirect_to users_show_path(user)
+      log_in(user)
+      redirect_to current_user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
