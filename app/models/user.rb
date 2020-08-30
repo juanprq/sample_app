847807e9dd298ac17d56560109d565ddda81efdf
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-08-30 13:34:18 -0500

    adds forget to sesions helper

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,10 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
   def authenticated?(remember_token)
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
