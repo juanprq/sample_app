d2f51b5fabc64a53e1f6b5ad0847b92898711721
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-08-29 20:20:34 -0500

    adds integration test for user valid login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -10,4 +10,15 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
+
+  test 'valid login' do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: { session: { email: 'foo@bar.com', password: '123456' } }
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select 'a[href=?]', login_path, count: 0
+    assert_select 'a[href=?]', user_path
+    assert_select 'a[href=?]', logout_path
+  end
 end
