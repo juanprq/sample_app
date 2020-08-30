4d837894311bae28983a061ee7ea602de0899309
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-08-30 13:39:03 -0500

    adds validation to logout action

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -34,5 +34,12 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_select 'a[href=?]', login_path
     assert_select 'a[href=?]', logout_path, count: 0
+
+    # simulate another window
+    delete logout_path
+    follow_redirect!
+    assert_select 'a[href=?]', login_path
+    assert_select 'a[href=?]', logout_path, count: 0
+    assert_select 'a[href=?]', user_path(@user), count: 0
   end
 end
