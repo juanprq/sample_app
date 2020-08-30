fb41cd618a1dfa01ad8a54d3879c1dbda0d659c3
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-08-29 20:22:52 -0500

    adds improvements to user valid login integration test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:foo)
+  end
+
   test 'invalid login' do
     get login_path
     assert_template 'sessions/new'
@@ -15,10 +19,11 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get login_path
     assert_template 'sessions/new'
     post login_path, params: { session: { email: 'foo@bar.com', password: '123456' } }
+    assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
     assert_select 'a[href=?]', login_path, count: 0
-    assert_select 'a[href=?]', user_path
+    assert_select 'a[href=?]', user_path(@user)
     assert_select 'a[href=?]', logout_path
   end
 end
