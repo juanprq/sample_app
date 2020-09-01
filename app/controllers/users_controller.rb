430fd9ae2063b05fea9dc2aa71409a4d7e340527
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-08-31 20:32:23 -0500

    adds current_user? method to sessions_helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,7 +49,7 @@ class UsersController < ApplicationController
     end
 
     def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_url) unless @user == current_user
+      user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(user)
     end
 end
