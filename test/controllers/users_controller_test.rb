65e648e49d254e67ae916015e32992faa6704adc
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-08-31 20:17:22 -0500

    adds authorization tests to users controller

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,13 +1,33 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
+
+  def setup
+    @user = users(:foo)
+  end
+
+  test 'should get new' do
     get signup_path
     assert_response :success
   end
 
-  test "should get show" do
-    get user_path users(:foo).id
+  test 'should get show' do
+    get user_path @user.id
     assert_response :success
+  end
+
+  test 'should redirect edit when not logger in' do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test 'should redirect update when not logged in' do
+    patch user_path(@user), params: {
+      user: { name: @user.name, email: @user.email }
+    }
+
+    assert_not flash.empty?
+    assert_redirected_to login_url
   end
 end
