a681b1553b88260418b7ed88d8bbc3b2ee9b2e84
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-09-02 18:24:35 -0500

    adds activation logic for link

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user&.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password]) && user&.activated?
       log_in(user)
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_back_or(user)
