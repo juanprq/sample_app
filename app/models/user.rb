f7193159317b3a90d6afa089dbed10a40c81efc2
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-09-01 19:41:32 -0500

    adds activation info to seeds and fixtures

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   has_secure_password
@@ -8,7 +8,8 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
-  before_save { self.email = email.downcase }
+  before_save :downcase_email
+  before_create :create_activation_digest
 
   def remember
     self.remember_token = User.new_token
@@ -32,5 +33,16 @@ class User < ApplicationRecord
 
   def User.new_token
     SecureRandom.urlsafe_base64
+  end
+
+  private
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
+
+  def downcase_email
+    self.email = email.downcase
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,9 @@ User.create!(
   email: 'example@railstutorial.com',
   password: 'password',
   password_confirmation: 'password',
-  admin: true
+  admin: true,
+  activated: true,
+  activated_at: Time.zone.now
 )
 
 99.times do |n|
@@ -11,6 +13,8 @@ User.create!(
     name: Faker::Name.name,
     email: "example-#{n}@railstutorial.com",
     password: 'password',
-    password_confirmation: 'password'
+    password_confirmation: 'password',
+    activated: true,
+    activated_at: Time.zone.now
   )
 end
