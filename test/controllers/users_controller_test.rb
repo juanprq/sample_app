4590aba332010e47228eff2c82c8998cecb09887
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-09-01 19:14:37 -0500

    adds integration tests for index view

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -83,4 +83,13 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     end
     assert_redirected_to root_path
   end
+
+  test 'should delete a user when logged-in as admin' do
+    log_in_as(@user)
+    assert_difference 'User.count', -1 do
+      delete user_path(@user)
+    end
+    assert_not flash.empty?
+    assert_redirected_to users_path
+  end
 end
--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -4,9 +4,10 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:foo)
+    @non_admin = users(:archer)
   end
 
-  test 'index includes pagination' do
+  test 'index as admin includes pagination and delete links' do
     log_in_as(@user)
     get users_path
     assert_template 'users/index'
@@ -14,6 +15,16 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     User.paginate(page: 1).each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
     end
+
+    assert_difference 'User.count', -1 do
+      delete user_path(@non_admin)
+    end
+  end
+
+  test 'index as non-admin' do
+    log_in_as(@non_admin)
+    get users_path
+    assert_select 'a', text: 'delete', count: 0
   end
 
 end
