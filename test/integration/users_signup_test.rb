1408b53cf51e3fe1bbbc6752fe5c50a29bd51b5b
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-09-03 20:39:11 -0500

    adds integration test to check for user activation

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  test 'invalid sign up' do
+
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
+  test 'invalid signup' do
     get signup_path
     assert_select 'input[id=?]', 'user_name', count: 1
     assert_no_difference 'User.count' do
@@ -12,13 +17,28 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.alert.alert-danger', count: 1, value: 'The form contains 6 errors'
   end
 
-  test 'valid sign up' do
+  test 'valid signup with account activation' do
     assert_difference 'User.count', 1 do
       post users_path, params: { user: { name: 'foo', email: 'foo-1@bar.com', password: '1' * 6, password_confirmation: '1' * 6 } }
     end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    # get the instance variable
+    user = assigns(:user)
+    assert_not user.activated?
+
+    log_in_as(user)
+    assert_not is_logged_in?
+
+    get edit_accounts_activation_path('invalid token', email: user.email)
+    assert_not is_logged_in?
+
+    get edit_accounts_activation_path(user.activation_token, email: 'wrong')
+    assert_not is_logged_in?
+
+    get edit_accounts_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
+
     follow_redirect!
-    # assert_template 'users/show'
-    assert_not flash.empty?
-    # assert is_logged_in?
+    assert is_logged_in?
   end
 end
