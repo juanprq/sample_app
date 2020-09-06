ea1272d151832ae8c325138daae9d23bb6b28238
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-09-05 21:18:10 -0500

    adds microposts tests

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:foo)
+    @micropost = Micropost.new(content: 'Lorem Ipsum', user_id: @user.id)
+  end
+
+  test 'should be valid' do
+    assert @micropost.valid?
+  end
+
+  test 'should required user_id' do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
+
 end
