3670fe2e17761ab15d64eaf2125a5802e69e2328
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-09-06 18:15:28 -0500

    adds seeders for microposts

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,3 +18,9 @@ User.create!(
     activated_at: Time.zone.now
   )
 end
+
+users = User.order(:created_at).take(6)
+50.times do |n|
+  content = Faker::Lorem.sentence(word_count: 5)
+  users.each { |user| user.microposts.create!(content: content) }
+end
