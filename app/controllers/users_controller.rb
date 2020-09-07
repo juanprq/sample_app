0773f71893eeca5fa4fc3d7383cf6dbbfec9d8b5
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-09-06 22:18:06 -0500

    adds tests for the microposts controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,24 +50,16 @@ class UsersController < ApplicationController
 
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 
-    def logged_in_user
-      unless logged_in?
-        flash[:danger] = 'Please log in.'
-        store_location
-        redirect_to login_url
-      end
-    end
+  def correct_user
+    user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(user)
+  end
 
-    def correct_user
-      user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(user)
-    end
-
-    def admin_user
-      redirect_to(root_path) unless current_user.admin?
-    end
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
+  end
 end
--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class MicropostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @micropost = microposts(:orange)
+  end
+
+  test 'should redirect create when not logged in' do
+    assert_no_difference 'Micropost.count' do
+      post microposts_path, params: { micropost: { content: 'Lorem Ipsum' } }
+    end
+
+    assert_redirected_to login_path
+  end
+
+  test 'should redirect destroy when not logged in' do
+    assert_no_difference 'Micropost.count' do
+      delete micropost_path(@micropost)
+    end
+
+    assert_redirected_to login_path
+  end
+
 end
