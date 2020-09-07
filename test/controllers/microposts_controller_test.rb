cbec5357a99024e00459e3b395dfecf8d7fd8a15
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-09-06 22:52:58 -0500

    adds tests for deleting another user micropost

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -22,4 +22,14 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_path
   end
 
+  test 'should redirect destroy the wrong micropost' do
+    log_in_as(users(:foo))
+    micropost = microposts(:ants)
+    assert_no_difference 'Micropost.count' do
+      delete micropost_path(micropost)
+    end
+
+    assert_redirected_to root_url
+  end
+
 end
