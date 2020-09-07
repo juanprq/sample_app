f86319b126218f3ef729ceef8eecf132aed41b76
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-09-06 23:01:33 -0500

    adds integration test for microposts

---
+++ test/integration/microposts_interface_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+
+class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:foo)
+  end
+
+  test 'micropost interface' do
+    log_in_as(@user)
+    get root_path
+
+    assert_select 'div.pagination'
+    assert_no_difference 'Micropost.count' do
+      post microposts_path, params: { micropost: { content: '  ' } }
+    end
+    assert_select 'div#error_explanation'
+    assert_select 'a[href=?]', '/?page=2'
+
+    content = 'This micropos really ties the room together'
+    assert_difference 'Micropost.count', 1 do
+      post microposts_path, params: { micropost: { content: content } }
+    end
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_match content, response.body
+
+    assert_select 'a', text: 'delete'
+    first_micropost = @user.microposts.paginate(page: 1).first
+    assert_difference 'Micropost.count', -1 do
+      delete micropost_path(first_micropost)
+    end
+
+    get user_path(users(:archer))
+    assert_select 'a', text: 'delete', count: 0
+  end
+
+end
