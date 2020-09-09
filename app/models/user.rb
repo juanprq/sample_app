6bf8e90d965c1eb1b09abfcd159779f92882ab29
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-09-09 18:02:20 -0500

    minor refactor to feed method

--- app/models/user.rb
+++ app/models/user.rb
@@ -80,7 +80,8 @@ class User < ApplicationRecord
   end
 
   def feed
-    Micropost.where('user_id IN (?) OR user_id = ?', following_ids, id)
+    following_ids = 'SELECT followed_id FROM relationships WHERE followed_id = :user_id'
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
   end
 
   private
